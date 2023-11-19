579e13ca6cedcf221419d2b8bd57265f7f2125a4
Gabriel Rocha Pessoa <gabrielrocha828@gmail.com>
2023-11-19 15:32:40 +1000

    chore: improvements and adjustments on jest.config, huskyrc and lintstagged

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 /** @type {import('jest').Config} */
 const config = {
-  root: ['<rootDir>/src'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
