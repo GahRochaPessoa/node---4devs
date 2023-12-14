4e16e618e4b554189d7fb07095d78dc73dec4b53
Gabriel Rocha Pessoa <gabrielrocha828@gmail.com>
2023-12-14 12:27:25 +1000

    feat: ensure AccountMOngoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { MongoClient } from 'mongodb'
+import { type Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
   async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL!, {
-    })
+    this.client = await MongoClient.connect(process.env.MONGO_URL!)
   },
 
   async disconnect(): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
